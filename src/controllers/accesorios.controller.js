84705aabaed5f6a386b9b2a6bc171fcbde65559c
WalterJoel <walter.valdivia@ucsp.edu.pe>
2022-11-21 21:08:27 -0500

    S

--- src/controllers/accesorios.controller.js
+++ src/controllers/accesorios.controller.js
@@ -27,22 +27,22 @@ export const createNewAccesorio = async (req, res) => {
     try {
         //Concatenar con la letra de la tabla
         const id_montura = v4() + codeForTables.tablaAccesorios;
-        const {habilitado,id_sede,cantidad,fecha_creacion_monturas,fecha_modificacion_monturas,precio_montura_c,precio_montura_v} = (req.body);
-        const datosMontura = {
+        const {habilitado,id_sede,cantidad,fecha_creacion_accesorio,fecha_modificacion_accesorio,precio_accesorio_c,precio_accesorio_v} = (req.body);
+        const datosAccesorio = {
             id_montura,
             cantidad,
             id_sede,
             habilitado,
-            fecha_creacion_monturas,
-            fecha_modificacion_monturas,
+            fecha_creacion_accesorio,
+            fecha_modificacion_accesorio,
             precio_montura_c,
             precio_montura_v,
         }
-        const newMontura = await dynamoClient.put({
+        const newAccesorio = await dynamoClient.put({
             TableName: TABLE_NAME_MONTURA,
-            Item: datosMontura
+            Item: datosAccesorio
         }).promise()
-        res.json(newMontura);       
+        res.json(newAccesorio);       
     } catch (error) {
         return res.status(500).json({ 
             message:error
