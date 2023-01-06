06d0324fa14fafb0b3c1fe58ffe1a5a9e962b7eb
WalterJoel <walter.valdivia@ucsp.edu.pe>
2023-01-05 22:04:31 -0500

    Funcion Insertar Productos desde Excel

--- src/app.js
+++ src/app.js
@@ -20,6 +20,9 @@ app.use(cors());
 //Paso esta funcion para que express entienda cuando se le envia un Json
 app.use(express.json());
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
 /* *********** MUY IMPORTANTE DEFINIRLO ANTES DE LLAMAR A LAS RUTAS PARA HABILITAR EL CORS */
 app.use(function (req, res, next) {
 
