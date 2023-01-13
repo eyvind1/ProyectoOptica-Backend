fc8e40792769c028a45773159b730119d41c2775
WalterJoel <walter.valdivia@ucsp.edu.pe>
2023-01-12 21:52:51 -0500

    cambiando codigo interno

--- src/controllers/monturas.controller.js
+++ src/controllers/monturas.controller.js
@@ -42,8 +42,8 @@ export const createNewMontura = async (req, res) => {
     try {
         const id_montura = v4() + codeForTables.tablaMonturas;
         //Genero el codigo interno para el codigo de barras
-        const nanoid = customAlphabet('1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-        let codigo_interno = nanoid(6) + prefixesForProducts.ProdMontura; 
+        const nanoid = customAlphabet('1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ',6)
+        let codigo_interno = nanoid() + prefixesForProducts.ProdMontura; 
         const {id_sede,tipo,habilitado,color,cantidad,codigo,fecha_creacion_monturas,fecha_modificacion_monturas, marca, material, precio_montura_c,precio_montura_v, talla} = (req.body);
         const datosMontura = {
             id_montura,
