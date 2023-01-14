9488548c71c5d0d15759d05710fb71738db8270f
WalterJoel <walter.valdivia@ucsp.edu.pe>
2023-01-14 13:00:09 -0500

    S

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    //res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
