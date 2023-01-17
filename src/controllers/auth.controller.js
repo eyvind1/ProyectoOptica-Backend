45581a25804066e136461e16be4fcf94204a394e
WalterJoel <walter.valdivia@ucsp.edu.pe>
2023-01-16 21:55:03 -0500

    agregando tiempo de expiracion y validando usuario

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -19,80 +19,64 @@ const TABLE_NAME_USUARIO  = "Usuarios";
 //export const responseSignIn = (req, res) => res.send("login OK");
 
 /* Por el momento solo valido el email  OJO  ************* */ 
+/* Validamos que el usuario este habilitado */ 
 async function findUserByEmail(usuario,contrasenia){
     const dynamoClient = new AWS.DynamoDB.DocumentClient();
     try {
         const paramsUsuario = {
             TableName: TABLE_NAME_USUARIO,
             FilterExpression:
-              'usuario = :usuario',
+              'usuario = :usuario and habilitado = :estado',
             ExpressionAttributeValues: {
-                ":usuario": usuario
+                ":usuario": usuario,
+                ":estado" : true,
             }
         };
         const user  = await dynamoClient.scan(paramsUsuario).promise();  
-        try {
-            const paramsPersona = {
-                TableName: 'Persona',
-                FilterExpression:
-                  'id_persona = :id_persona',
-                ExpressionAttributeValues: {
-                    ":id_persona": user.Items[0].id_persona
-                }
-            };
-            let result= await dynamoClient.scan(paramsPersona).promise();      
-            user.Items[0].nombres = result.Items[0].nombres;
-            user.Items[0].apellidos = result.Items[0].apellidos;
-            //Agrego atributos de la persona al json usuario
-            return user.Items;
-
-        } catch (error) {
-            console.log(error);
-            return error;
+        console.log(user);
+        
+        //En este caso utilizamos scan y sino encuentran no retorna error simplemente retora un vacio por eso valido
+        if(user.Items.length>0){
+            try {
+                const paramsPersona = {
+                    TableName: 'Persona',
+                    FilterExpression:
+                      'id_persona = :id_persona' ,
+                    ExpressionAttributeValues: {
+                        ":id_persona": user.Items[0].id_persona
+                    }
+                };
+                let result= await dynamoClient.scan(paramsPersona).promise();      
+                user.Items[0].nombres = result.Items[0].nombres;
+                user.Items[0].apellidos = result.Items[0].apellidos;
+                //Agrego atributos de la persona al json usuario
+                return user.Items;
+    
+            } catch (error) {
+                console.log(error);
+                return error;
+            }
         }
+        //En otro caso retorno un array vacio
+        return user;
     } catch (error) {
         console.log(error);
         return error;
     }
 }
 
-
 export const signIn = async (req, res) => {
     const { email, password } = req.body;
-
     const user = await findUserByEmail(email,password);
-    console.log(user[0].id_usuario)
     //Si existe un usuario
     if (user.length> 0) {
-        const token = jwt.sign({_id: user[0].id_usuario}, 'secretkey',{expiresIn:'1h'});
+        const token = jwt.sign({_id: user[0].id_usuario}, 'secretkey',{expiresIn:'120s'});
         return res.status(200).json({token,user});
     }
     else{
         return res.status(401).send('The email doen\' exists');
     }
 };
-
-async function verifyToken(req, res, next) {
-	try {
-		if (!req.headers.authorization) {
-			return res.status(401).send('Unauhtorized Request');
-		}
-		let token = req.headers.authorization.split(' ')[1];
-		if (token === 'null') {
-			return res.status(401).send('Unauhtorized Request');
-		}
-
-		const payload = await jwt.verify(token, 'secretkey');
-		if (!payload) {
-			return res.status(401).send('Unauhtorized Request');
-		}
-		req.userId = payload.id_usuario;
-		next();
-	} catch(e) {
-		//console.log(e)
-		return res.status(401).send('Unauhtorized Request');
-	}
-}
 
 export const logOut = async (req, res, next) => {
     await req.logout((err) => {
